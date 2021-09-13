5e1166c022d9af3e2c1d52220a14b3df84730388
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-13 18:43:42 -0400

    update request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    // format the github api url
+    var apiUrl = "https://api.github.com/users" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function() {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
