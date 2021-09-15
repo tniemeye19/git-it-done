bb2338d7e3f5bac66f1c5f912e859a7e222fe466
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-09-14 20:15:04 -0400

    completed user-repos section. added url requests and catch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,14 +21,30 @@ var getUserRepos = function() {
     var apiUrl = "https://api.github.com/users" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function() {
-            displayRepos(data, user);
-        });
-    });
+    fetch(apiUrl)
+        .then(function(response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: GitHub User Not Found");
+            }
+        })
+        .catch(function(error) {
+            // Notice this `.catch()` getting chained onto the end of the `.then` method
+            alert("Unabel to connect to GitHub");
+        })
 };
 
 var displayRepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
